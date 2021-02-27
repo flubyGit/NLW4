6cf64267ad9d9c52e701a4b0193e8d1839107dec
felipe <profissionalf.andre@gmail.com>
2021-02-27 12:33:12 -0300

    feat: add audio, permission navigator and more

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,6 +1,9 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../../contexts/ChallengesContext";
 import styles from "../../styles/components/Profile.module.css";
 
 export function Profile(): JSX.Element {
+  const { level } = useContext(ChallengesContext);
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/flubyGit.png" alt="Felipe André" />
@@ -8,7 +11,7 @@ export function Profile(): JSX.Element {
         <strong>Felipe André</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
